8ace07597f33b3401e6749d1d5a143971e63c458
ajaychirme <ajaychirme84@gmail.com>
2023-05-12 16:43:13 +0530

    backend commit

---
+++ Server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const cors = require("cors");
+
+const routes = require("./routes/ToDoRoute");
+
+const app = express();
+const PORT = process.env.PORT | 5000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+    .connect(`mongodb+srv://${process.env.USERID}:${process.env.PASSWORD}@cluster0.fpj5vem.mongodb.net/todoassign?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Mongodb Connected..."))
+    .catch((err) => console.error(err));
+
+// Routes
+app.use(routes);
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
